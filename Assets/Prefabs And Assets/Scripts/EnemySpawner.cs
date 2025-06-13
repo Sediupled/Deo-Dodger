3c70032e0a7602707a075e1d6320458a75749f6a
Sediupled <chaitanya.rook@gmail.com>
2025-06-13 18:08:17 +0530

    Added some level design and entities

--- Assets/Prefabs And Assets/Scripts/EnemySpawner.cs
+++ Assets/Prefabs And Assets/Scripts/EnemySpawner.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Pathfinding;
+
 using UnityEngine;
 using Random = System.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] public int EnemQuantity = 5;
+    [SerializeField] public int EnemQuantity = 20;
     [SerializeField] public GameObject ePrefab;
     [SerializeField] public GameObject[] posArr;
     [SerializeField] public GameObject targ;
@@ -14,13 +14,14 @@ public class EnemySpawner : MonoBehaviour
     private int enemiesSpawned = 0;
     Vector3 arbPos;
     
+    Random rand = new Random();
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         while(enemiesSpawned < EnemQuantity)
         {
-            var rand = new Random();
+            
             int arbIndex = rand.Next(posArr.Length);
             if (saveIndex == arbIndex)
             {
@@ -28,7 +29,6 @@ public class EnemySpawner : MonoBehaviour
             }
             arbPos = posArr[arbIndex].transform.position;
             Instantiate(ePrefab, arbPos, Quaternion.identity);
-            ePrefab.GetComponent<AIDestinationSetter>().target = targ.transform;
             saveIndex = arbIndex;
             enemiesSpawned++;
         }
